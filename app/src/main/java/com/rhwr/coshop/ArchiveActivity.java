788f6721d63101f5a2d5b88cac309000893b2e5e
raissa mable <eyram-raissa.mable@efrei.net>
2025-04-14 15:17:39 +0200

    page archive

---
+++ app/src/main/java/com/rhwr/coshop/ArchiveActivity.java
@@ -0,0 +1,13 @@
+package com.rhwr.coshop;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArchiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive);
+    }
+}
+
